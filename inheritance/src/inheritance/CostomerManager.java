71809db4885db9740dec50342fe779c64c15f030
çağrı <ar@DESKTOP-QBOKPBM>
2022-09-28 19:55:37 +0300

    ...

---
+++ inheritance/src/inheritance/CostomerManager.java
@@ -0,0 +1,6 @@
+package inheritance;
+
+public class CostomerManager extends PersonManager{
+
+
+}
---
+++ inheritenceDemo/src/inheritenceDemo/Main.java
@@ -0,0 +1,12 @@
+package inheritenceDemo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        OgretmenKrediManager ogretmenKrediManager = new OgretmenKrediManager();
+        KrediUI krediUI = new KrediUI();
+        krediUI.KrediHesapla(new OgretmenKrediManager());
+
+    }
+
+}
